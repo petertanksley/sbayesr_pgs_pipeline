41f479179a99db2ac90b0482b24023b2357b0596
Peter Tanksley <peter.tanksley@austin.utexas.edu>
2023-04-18 13:42:08 -0500

    Completed first pass of PGS; need to check

--- code/non_cog_pgs_SBayesR.sh
+++ code/non_cog_pgs_SBayesR.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -J tt_pgs
 #SBATCH -o tt_pgs.o%j
-#SBATCH -N 2
+#SBATCH -N 1
 #SBATCH -n 24
-#SBATCH -p skx-normal
-#SBATCH -t 010:00:00
+#SBATCH -p icx-normal
+#SBATCH -t 05:00:00
 #SBATCH -A Developmental-Behavi
 
 #========================================================================================#
@@ -12,10 +12,12 @@
 #========================================================================================#
 
 export PATH=$PATH:/work2/07624/tankslpr/stampede2/TOOLS
+export PATH=$PATH:/work2/07624/tankslpr/stampede2/TOOLS/plink
+export PATH=$PATH:/work2/07624/tankslpr/stampede2/TOOLS/plink2
 export PATH=$PATH:/work2/07624/tankslpr/stampede2/TOOLS/gctb_2.04.3_Linux
 
 #========================================================================================#
-# OPERATIONS
+# Format GWAS effects (.ma format)
 #========================================================================================#
 
 #Convert sumstats to .ma format
@@ -23,31 +25,44 @@ export PATH=$PATH:/work2/07624/tankslpr/stampede2/TOOLS/gctb_2.04.3_Linux
 if [[	! -e ../temp/non_cog_grch37.ma || \
 	! -s ../temp/non_cog_grch37.ma ]]; then
 	gunzip ../input/GCST90011874_buildGRCh37.tsv.gz
-	echo "SNP A1 A2 freq b se p N" | tr -s " " "\t" > ../temp/non_cog_grch37.ma 
-	tail -n +2 ../input/GCST90011874_buildGRCh37.tsv | \
-	awk 'BEGIN{FS="\t"; OFS=FS}{print $1, $4, $5, $6, $7, $8}' | \
-	awk 'BEGIN{FS="\t"; OFS=FS}{$(NF+1) = 510795; print }'>> ../temp/non_cog_grch37.ma
+	awk 'BEGIN{FS="\t"; OFS=FS}{print $1, $4, $5, $6, $7, $8, $10, 510795}' ../input/GCST90011874_buildGRCh37.tsv > ../temp/non_cog_grch37.ma
 	gzip ../input/GCST90011874_buildGRCh37.tsv
 fi
 
-#decompress ld matrix
-#unzip ../../TOOLS/gctb_2.04.3_Linux/ld_matrix/ukb_50k_bigset_2.8M.zip -d ../../TOOLS/gctb_2.04.3_Linux/ld_matrix
+#========================================================================================#
+# Run SBayesR. Steps: unzip ld-mat; format list; run SBayesR; zip ld-mat
+#========================================================================================#
 
-ls ../../ukb_50k_bigset_2.8M/*.bin | \
-	sed -r 's/.{4}$//' > ../temp/ld_mat_list.txt
+if [[	! -e ../output/non_cog_sbayesr_ukb2.8M.snpRes || \
+	! -s ../output/non_cog_sbayesr_ukb2.8M.snpRes ]]; then
+	unzip ../../ukb_ref_panels/ukb_50k_bigset_2.8M.zip; rm ../../ukb_ref_panels/ukb_50k_bigset_2.8M.zip
+	old_path="/gpfs1/scratch/group30days/cnsg_park/uqllloyd/sbayesr/post_review/ukb_50k_bigset_2.7M/ukb_50k_bigset_2.8M/"
+        new_path="/scratch/07624/tankslpr/ukb_ref_panels/ukb_50k_bigset_2.8M/"
+        sed "s#$old_path#$new_path#g" ../../ukb_ref_panels/ukb_50k_bigset_2.8M/ukb50k_2.8M_shrunk_sparse.mldmlist \
+        > ../../ukb_ref_panels/ukb_50k_bigset_2.8M/ukb50k_2.8M_shrunk_sparse_updated.mldmlist
+	gctb	--sbayes R \
+		--mldm ../../ukb_ref_panels/ukb_50k_bigset_2.8M/ukb50k_2.8M_shrunk_sparse_updated.mldmlist \
+		--gwas-summary ../temp/non_cog_grch37.ma \
+		--exclude-mhc \
+		--seed 12345 \
+		--impute-n \
+		--pi 0.95,0.02,0.02,0.01 \
+		--gamma 0.0,0.01,0.1,1 \
+		--chain-length 10000 \
+		--burn-in 2000 \
+		--out-freq 10 \
+		--out ../output/non_cog_sbayesr_ukb2.8M
+	zip -rm ../../ukb_ref_panels/ukb_50k_bigset_2.8M.zip ../../ukb_ref_panels/ukb_50k_bigset_2.8M
+fi
 
-#run SBayesR
-gctb	--sbayes R \
-	--mldm ../temp/ld_mat_list.txt \
-	--gwas-summary ../temp/non_cog_grch37.ma \
-	--pi 0.95,0.02,0.02,0.01 \
-	--gamma 0.0,0.01,0.1,1 \
-	--exclude-mhc \
-	--hsq 0.5 \
-	--chain-length 25000 \
-	--burn-in 5000 \
-	--seed 12345 \
-	--thread 48 \
-	--no-mcmc-bin \
-	--impute-n \
-	--out ../output/non_cog_sbayesr
+#========================================================================================#
+# Compute PGS with Plink
+#========================================================================================#
+
+target="/work2/07624/tankslpr/stampede2/TTP_GENOTYPES/IMPUTED_1KG/TEXAS_TWINS.1KG.EUR_noOutliers.chr1_22.updated.SNPs.maf1e-3.hwe1e-6.info90"
+snpRes="../output/non_cog_sbayesr_ukb2.8M.snpRes"
+
+
+plink 	--bfile $target \
+	--score $snpRes 2 5 8 header sum center \
+	--out ../output/ttp_non_cog_sbayesr_ukb2.8M
